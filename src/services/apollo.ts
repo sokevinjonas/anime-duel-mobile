9f178bf5e2ea97fb63857b763aeaec2bb45a9e6a
sokevinjonas <kjonasdevpro@gmail.com>
2026-06-28 18:54:01 +0000

    feat: update Apollo client configuration to use environment variables for API URLs

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -1,17 +1,16 @@
-import { ApolloClient, InMemoryCache, createHttpLink, from } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client';
+import { HttpLink } from '@apollo/client/link/http';
 import { setContext } from '@apollo/client/link/context';
 import { Platform } from 'react-native';
 import { getAccessToken } from './auth';
 
 const getApiUrl = () => {
-  if (Platform.OS === 'web') return 'http://localhost:3001/graphql';
-  // Android emulator uses 10.0.2.2 to reach host machine
-  if (Platform.OS === 'android') return 'http://10.0.2.2:3001/graphql';
-  // iOS simulator uses localhost
-  return 'http://localhost:3001/graphql';
+  if (Platform.OS === 'web') return process.env.EXPO_PUBLIC_API_URL_WEB;
+  if (Platform.OS === 'android') return process.env.EXPO_PUBLIC_API_URL_ANDROID;
+  return process.env.EXPO_PUBLIC_API_URL_IOS;
 };
 
-const httpLink = createHttpLink({
+const httpLink = new HttpLink({
   uri: getApiUrl(),
 });
 
@@ -26,6 +25,6 @@ const authLink = setContext(async (_, { headers }) => {
 });
 
 export const apolloClient = new ApolloClient({
-  link: from([authLink, httpLink]),
+  link: ApolloLink.from([authLink, httpLink]),
   cache: new InMemoryCache(),
 });
